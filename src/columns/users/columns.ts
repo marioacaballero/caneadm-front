5030ad48eacbfe62d66be96d6c3e26e8ccac0797
Mario Caballero <mario.caballero.948@gmail.com>
2023-03-27 22:24:49 -0300

    add clients table

---
+++ src/columns/users/columns.ts
@@ -0,0 +1,22 @@
+import { Column } from "../../interfaces/users/column";
+
+export const columns: readonly Column[] = [
+  { id: "id", label: "id", minWidth: 170 },
+  { id: "firstName", label: "Nombre", minWidth: 170 },
+  { id: "lastName", label: "Apellido", minWidth: 100 },
+  {
+    id: "email",
+    label: "Email",
+    minWidth: 170,
+  },
+  {
+    id: "cell",
+    label: "Celular",
+    minWidth: 170,
+  },
+  {
+    id: "address",
+    label: "Dirección",
+    minWidth: 170,
+  },
+];
---
+++ src/components/StickyHeadTable.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { FetchType } from "@/interfaces/fetchHook/fetchData";
+import { fetchHook } from "@/hooks/fetchHook";
+import { DotWave } from "@uiball/loaders";
+
+export default function StickyHeadTable({ url, columns }: FetchType) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rows, setRows] = React.useState([]);
+
+  fetchHook(url, setRows);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column: any) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.length ? (
+              rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row: any) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                      {columns.map((column: any) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === "number"
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })
+            ) : (
+              <DotWave size={47} speed={1} color="black" />
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
